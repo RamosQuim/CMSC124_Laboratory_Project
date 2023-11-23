8fb999d6a36516318bf0b20d20f9fbb014bdba94
RamosQuim <qlramos@up.edu.ph>
2023-11-23 16:59:29 +0800

    chore:removed some unnecessary code

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -5,7 +5,6 @@ from  tkinter import ttk
 import tkinter.font as font
 from tkinter import scrolledtext
 from tkinter import filedialog as fd
-from tkinter import messagebox
 import ctypes as ct
 
 def filename():
@@ -15,8 +14,8 @@ def filename():
 
     filename = fd.askopenfilename(filetypes=filetypes)
     file = open(filename, "r")
-    text_area.delete("1.0", "end")
-    text_area.insert("end", file.read(), ("centered",))
+    textEditor.delete("1.0", "end")
+    textEditor.insert("end", file.read(), ("centered",))
     
     file.close()
 
@@ -29,20 +28,20 @@ set_window_attribute = ct.windll.dwmapi.DwmSetWindowAttribute
 get_parent = ct.windll.user32.GetParent
 set_window_attribute(get_parent(root.winfo_id()), 20, ct.byref(ct.c_int(2)), 4)
 
-open0 = tk.Button(root, text='Open File', font=font.Font(size = 10), bd=1, bg='#365963', fg='white', command=lambda:filename())
-open0.grid(row=0, column=0, padx=5, pady=5, sticky="NSEW")
+openButton = tk.Button(root, text='Open File', font=font.Font(size = 10), bd=1, bg='#365963', fg='white', command=lambda:filename())
+openButton.grid(row=0, column=0, padx=5, pady=5, sticky="NSEW")
 
-open1 = Label(text = "LOL CODE Interpreter", font=font.Font(size = 12, weight='bold'), fg='white',bg='#0c1818')
-open1.grid(row=0, column=1, padx=5, pady=5, columnspan=2, sticky='W')
+title = Label(text = "LOL CODE Interpreter", font=font.Font(size = 12, weight='bold'), fg='white',bg='#0c1818')
+title.grid(row=0, column=1, padx=5, pady=5, columnspan=2, sticky='W')
 
-open1 = Label(text = "Lexemes", font=font.Font(size = 12), fg='white', bg='#0c1818', borderwidth=1, relief="ridge")
-open1.grid(row=1, column=1, padx=5, sticky='NSEW')
+lexemeHeader = Label(text = "Lexemes", font=font.Font(size = 12), fg='white', bg='#0c1818', borderwidth=1, relief="ridge")
+lexemeHeader.grid(row=1, column=1, padx=5, sticky='NSEW')
 
-open1 = Label(text = "Symbol Table", font=font.Font(size = 12), fg='white', bg='#0c1818', borderwidth=1, relief="ridge")
-open1.grid(row=1, column=2, padx=5, sticky='NSEW')
+symbolHeader = Label(text = "Symbol Table", font=font.Font(size = 12), fg='white', bg='#0c1818', borderwidth=1, relief="ridge")
+symbolHeader.grid(row=1, column=2, padx=5, sticky='NSEW')
 
-text_area = scrolledtext.ScrolledText(root, width = 33, height = 15, bg='#193433', fg='white')
-text_area.grid(row=1, column=0, padx=5, pady=5, rowspan=2, sticky="NSEW")
+textEditor = scrolledtext.ScrolledText(root, width = 33, height = 15, bg='#193433', fg='white')
+textEditor.grid(row=1, column=0, padx=5, pady=5, rowspan=2, sticky="NSEW")
 
 lexemes = ttk.Treeview(root, selectmode='browse', height=15)
 lexemes.grid(row=2, column=1, padx=5, pady=5)
@@ -54,22 +53,23 @@ lexemes.heading("#0",text="",anchor=CENTER)
 lexemes.heading("lexeme",text="Lexeme",anchor=CENTER)
 lexemes.heading("classification",text="Classification",anchor=CENTER)
 
-lexemes = ttk.Treeview(root, selectmode='browse', height=15)
-lexemes.grid(row=2, column=2, padx=5, pady=5)
-lexemes['columns'] = ('identifier', 'value')
-lexemes.column("#0", width=0,  stretch=NO)
-lexemes.column("identifier",anchor=CENTER, width=165,stretch=NO)
-lexemes.column("value",anchor=CENTER, width=165,stretch=NO)
-lexemes.heading("#0",text="",anchor=CENTER)
-lexemes.heading("identifier",text="Identifier",anchor=CENTER)
-lexemes.heading("value",text="Value",anchor=CENTER)
+symbolTable = ttk.Treeview(root, selectmode='browse', height=15)
+symbolTable.grid(row=2, column=2, padx=5, pady=5)
+symbolTable['columns'] = ('identifier', 'value')
+symbolTable.column("#0", width=0,  stretch=NO)
+symbolTable.column("identifier",anchor=CENTER, width=165,stretch=NO)
+symbolTable.column("value",anchor=CENTER, width=165,stretch=NO)
+symbolTable.heading("#0",text="",anchor=CENTER)
+symbolTable.heading("identifier",text="Identifier",anchor=CENTER)
+symbolTable.heading("value",text="Value",anchor=CENTER)
 
-open3 = tk.Button(root, text='EXECUTE', font=font.Font(size = 10), bd=1, bg='#365963', fg='white')
-open3.grid(row=3, column=0, padx=5, pady=5, columnspan=3, sticky="NSEW")
+executeButton = tk.Button(root, text='EXECUTE', font=font.Font(size = 10), bd=1, bg='#365963', fg='white')
+executeButton.grid(row=3, column=0, padx=5, pady=5, columnspan=3, sticky="NSEW")
 
 text_area4 = scrolledtext.ScrolledText(root, wrap = tk.WORD, height = 15, fg='white', bg='#193433')
 text_area4.grid(row=4, column=0, padx=5, pady=5, columnspan=3, sticky="NSEW")
 
+# style of tables
 style = ttk.Style(root)
 style.theme_use("clam")
 style.configure("Treeview.Heading", background="#365963", foreground="white", relief="flat")
